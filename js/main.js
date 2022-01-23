2540ccbe822def3958699ea670000c66964dec0a
B-Billings <b_billings@fanshaweonline.ca@users.noreply.github.com>
2022-01-23 16:53:50 -0500

    added some js and ids to images

---
+++ js/main.js
@@ -0,0 +1,15 @@
+(() => {
+    let theHeading = document.querySelector(".main-heading");
+    theIcons = document.querySelectorAll(".icon");
+
+    function logElement() {
+        console.log('clicled on an element', this.id);
+}
+
+theHeading.addEventListener("click", logElement);
+
+theIcons.forEach (icon => icon.addEventListener("click", logElement));
+    
+
+
+})();
